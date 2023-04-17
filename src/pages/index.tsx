910abcc633f33dfd3427b7712a3c3b1eed41aede
Yvan Brito <yvanbrito96@gmail.comm>
2023-04-17 11:01:26 -0300

    adjusting colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { Roboto, Chivo_Mono } from "next/font/google";
 import Link from "next/link";
 import Image from "next/image";
+import Head from "next/head";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 import perfilImg from "../assets/perfil.png";
 
 const roboto = Roboto({
-  weight: "400",
+  weight: ["400", "700"],
   subsets: ["latin"],
 });
 const chivo = Chivo_Mono({
@@ -17,7 +18,10 @@ const chivo = Chivo_Mono({
 export default function Home() {
   return (
     <main>
-      <header className="flex fade-in-header items-center justify-end absolute w-full h-14 py-3 px-10">
+      <Head>
+        <title>Yvan Brito - Frontend Developer</title>
+      </Head>
+      <header className="flex fade-in-header items-center justify-end absolute w-full h-14 mt-7 py-3 px-10">
         <nav className="inline-flex">
           <button
             onClick={() => {
@@ -30,7 +34,7 @@ export default function Home() {
             <GiHamburgerMenu className="inline w-8 h-8 rounded-sm hover:bg-gray-400 sm:hidden" />
           </button>
           <ul
-            className="mobile-menu hidden flex-col gap-12 absolute top-16 right-16 p-5 bg-white border border-gray-400 rounded-md sm:bg-transparent sm:p-0 sm:border-0 sm:flex sm:flex-row sm:static"
+            className="mobile-menu hidden flex-col gap-12 absolute top-16 right-16 p-5 bg-white border border-gray-400 rounded-md sm:bg-transparent sm:p-0 sm:border-0 sm:mr-36 sm:flex sm:flex-row sm:static"
             id="navbar-default"
           >
             <li className="mb-2">
@@ -52,7 +56,7 @@ export default function Home() {
         </nav>
       </header>
       <section>
-        <main className="h-screen pt-14 px-6 flex flex-col justify-center bg-gradient-to-b from-[#151a24] to-[#29364d] items-center sm:px-12 lg:px-48 lg:flex-row-reverse">
+        <main className="h-screen pt-14 px-6 flex flex-col justify-center bg-[#29364d] items-center sm:px-12 lg:px-48 lg:flex-row-reverse">
           <div className="fade-in-photo flex items-center justify-center w-2/3 mb-24">
             <Image
               className="rounded-full"
@@ -62,25 +66,32 @@ export default function Home() {
             />
           </div>
           <div className="flex flex-col 2xl:w-2/3">
-            <span className={`${chivo.className} fade-in-1 mb-8`}>
+            <span className={`${chivo.className} fade-in-1 text-gray-200 mb-8`}>
               Hi, my name is
             </span>
             <h1
-              className={`${roboto.className} fade-in-2 text-7xl font-bold lg:text-9xl`}
+              className={`${roboto.className} fade-in-2 text-[#c6d2e7] text-7xl font-bold lg:text-7xl`}
             >
-              Yvan Brito
+              Yvan Brito.
             </h1>
             <span
-              className={`${roboto.className} fade-in-3 text-gray-400 text-2xl lg:text-3xl`}
+              className={`${roboto.className} fade-in-3 mb-8 text-[#c6d2e7] text-2xl lg:text-3xl`}
             >
-              Building awesome applications since 2018
+              Building awesome applications since 2018.
             </span>
             <span
-              className={`${chivo.className} fade-in-4 text-gray-400 mt-8 2xl:w-4/5`}
+              className={`${chivo.className} fade-in-4 text-[#c6d2e7] 2xl:w-4/5`}
             >
               I&apos;m a Frontend Developer specialized in developing awesome
-              digital experience. I&apos;m currently working for Pagbank
-              PagSeguro building services for backoffice and advisory systems.
+              digital experience. I&apos;m currently working for{" "}
+              <a
+                href="https://pagseguro.uol.com.br/"
+                target="_blank"
+                className="text-[#799434] hover:underline hover:text-[#B4E043]"
+              >
+                Pagbank PagSeguro
+              </a>{" "}
+              building services for backoffice and advisory systems.
             </span>
           </div>
         </main>
